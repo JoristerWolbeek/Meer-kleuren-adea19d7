a420309b103441f3683be2d8f20407d1c8e76831
Joris ter Wolbeek <21074092@talnet.nl>
2020-03-09 14:12:45 +0100

    Uitwerking van de exercise

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-function color(){
-    document.body.style.backgroundColor = "red";
+var colorBackground;
+var colorTitel;
+
+function backgroundColorOpslaan(){
+    colorBackground = document.getElementById("which").value;
+}
+
+function titleColorOpslaan(){
+    colorTitel = document.getElementById("which").value;
+}
+
+function colorIn(){
+    document.body.style.backgroundColor = colorBackground ;
+    document.querySelector("h1").style.color = colorTitel ;
 }
